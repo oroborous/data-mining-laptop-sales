aa26be063afdc1acc32cf6930dd89637c3902e92
Stacy Read <sread@wctc.edu>
2022-09-30 16:56:19 -0500

    Cleaned up comments and readme

--- README.md
+++ README.md
@@ -1,15 +1,19 @@
 # Laptop Sales
 
-Answers the question: How did the retail price of a laptop change over time, per the LaptopSales.csv data.
+Uses LaptopSales.csv data to answer the question: How did the retail price of a laptop change over time?
 
-# R Code for Chart
+Data is preprocessed using Python to categorize each laptop as "Lower End", "Mid Range", or "Top of the Line", based on the components it contains. e.g., hard drive size, amount of RAM, battery life, etc.
+
+# R Code for Plots
 `library(ggplot2)`
 
-`df <- read.csv("C:/Users/moogi/PycharmProjects/data-mining-laptop-sales/overall/LaptopSalesCategorized.csv", header=FALSE, stringsAsFactors=TRUE)`
+`df <- read.csv("LaptopSalesCategorized.csv", header=FALSE, stringsAsFactors=TRUE)`
 
 `names(df) <- c("month", "price", "power", "power.min", "power.max", "category", "ppd")`
 
 `df$mf <- as.factor(df$month)`
+
+The code below plots the average price per laptop category month-to-month as a line graph.
 
 `avg.price <- aggregate(df$price, list(df$month, df$category), FUN=mean)`
 
@@ -17,10 +21,20 @@ Answers the question: How did the retail price of a laptop change over time, per
 
 `p1 <- ggplot(data=avg.price, aes(x=month, y=price, group=category)) + geom_line() + labs(title="Retail Price of Laptops", x ="Month", y = "Retail price")`
 
+![Average retail price](PricePerCategory1.png "Average retail price")
+
+The code below plots the average purchasing power of the customer month-to-month as a line graph. That is, how much extra laptop power does the customer get for every dollar spent?
+
 `avg.ppd <- aggregate(df$ppd, list(df$month, df$category), FUN=mean)`
 
 `names(avg.ppd) <- c("month", "category", "ppd")`
 
 `p2 <- ggplot(data=avg.ppd, aes(x=month, y=ppd, group=category)) + geom_line() + labs(title="Customer Purchasing Power", x ="Month", y = "Power per dollar")`
 
+![Average purchasing power](PpdPerCategory1.png "Average purchasing power")
+
+The code below displays the summary statistics for each category of laptop, each month, as a group of boxplots.
+
 `p3 <- ggplot(df, aes(x=mf, y=price, fill=category)) + geom_boxplot() + labs(title="Retail Price of a Laptop (2008)", x ="Month", y = "Retail price")`
+
+![Summary stats per category](RetailPriceBoxplot-Monthly.png "Summary stats per category")
--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from pathlib import Path
 
+
+# Given a laptop's "power" score, categorize into
+# which third it falls: Lower End, Mid Range, or Top
+# of the Line
 def categorize(score, max_score, min_score):
     low_range = (max_score - min_score) * (1/3)
     mid_range = (max_score - min_score) * (2/3)
@@ -10,17 +14,22 @@ def categorize(score, max_score, min_score):
     else:
         return "Top of the Line"
 
+
+# Scale the given value to between 1 and 10
 def scale(old_max, old_min, old_value):
     new_max = 10
     new_min = 1
     old_range = (old_max - old_min)
     if old_range == 0:
+        # Don't divide by zero! Just return max
+        # if only one value in series
         return new_max
     else:
         new_range = (new_max - new_min)
         return (((old_value - old_min) * new_range) / old_range) + new_min
 
 
+# Find the max and min of each laptop attribute sold in each month
 def monthly_min_max():
     cols = ["month", "price", "screen", "battery", "ram", "proc", "wifi", "hd", "apps"]
     # mmm (monthly-min-max) is table of min/maxes
@@ -43,8 +52,8 @@ def monthly_min_max():
                 if col == 0:
                     month_row.append(row - 1)
                 else:
-                    month_row.append(9999) # default min
-                    month_row.append(-9999) # default max
+                    month_row.append(9999)  # default min
+                    month_row.append(-9999)  # default max
 
     file_path = Path.cwd() / "LaptopSales.csv"
 
@@ -55,13 +64,12 @@ def monthly_min_max():
             i += 1  # line number
             line = line.strip()
             vals = line.split(",")
+            # using uncleaned data file, so skip rows with
+            # missing date or price vals
             if i == 1 or vals[0] == "" or vals[4] == "":
                 x += 1
-                # print("Skipped", i, x)
             else:
-                # print(vals)
                 month = int(vals[0][:vals[0].index("/")])
-                # print(month)
 
                 price = int(vals[4])
                 mmm[month][1] = min(mmm[month][1], price)
@@ -96,15 +104,17 @@ def monthly_min_max():
             out.write(",".join([str(num) for num in row]))
             out.write("\n")
 
-    print(mmm)
     return mmm
 
+
+# Same as monthly_min_max, but looks at the whole year instead of
+# breaking out by month
 def overall_min_max():
     cols = ["price", "screen", "battery", "ram", "proc", "wifi", "hd", "apps"]
+
     # omm (overall-min-max) is table of min/maxes
     # price-min   price-max   screen-min  screen-max
     # 395         457         15          15
-
     omm = []
     for row in range(0, 2):
         omm_row = []
@@ -126,6 +136,8 @@ def overall_min_max():
             i += 1  # line number
             line = line.strip()
             vals = line.split(",")
+            # using uncleaned data file, so skip rows with
+            # missing date or price vals
             if i == 1 or vals[0] == "" or vals[4] == "":
                 x += 1
             else:
@@ -165,17 +177,14 @@ def overall_min_max():
     return omm
 
 
-    # for each laptop, rescale each component 1-10 to reflect relative "power" to other
-    # laptops sold in the same month
-
+# For each laptop, rescale each component 1-10 to reflect relative "power" to other
+# laptops sold in the same month
+def scaled_monthly_sales(mmm):
     # sc (scale) is table of each sale with laptop attributes scaled 1-10
     # and a total "power" rating that is the sum
-    # Each laptop is categorized as being "Low Range", "Mid Range", or "Top of the Line"
-    # based on how much power it had versus other laptops sold that month
-    # month price       screen      battery     ram     power   category
-    # 1     395         1           8.7         5.6     15.3    Mid Range
-    # 2     417         10          5.4         2.3     17.7    Low Range
-def scaled_monthly_sales(mmm):
+    # month price       power   screen      battery     ram
+    # 1     395         15.3    1           8.7         5.6
+    # 2     417         17.7    10          5.4         2.3
     sc = []
 
     file_path = Path.cwd() / "LaptopSales.csv"
@@ -187,16 +196,16 @@ def scaled_monthly_sales(mmm):
             i += 1  # line number
             line = line.strip()
             vals = line.split(",")
+            # using uncleaned data file, so skip rows with
+            # missing date or price vals
             if i == 1 or vals[0] == "" or vals[4] == "":
                 x += 1
-                # print("Skipped", i, x)
             else:
-                # print(vals)
                 month = int(vals[0][:vals[0].index("/")])
-                # print(month)
+
                 sc_row = []
                 sc.append(sc_row)
-                sum = 0
+                power = 0
                 sc_row.append(month)
 
                 price = int(vals[4])
@@ -206,74 +215,71 @@ def scaled_monthly_sales(mmm):
                 minv = mmm[month][3]
                 maxv = mmm[month][4]
                 sc_val = scale(maxv, minv, screen)
-                sum += sc_val
+                power += sc_val
                 sc_row.append(sc_val)
 
                 battery = int(vals[6])
                 minv = mmm[month][5]
                 maxv = mmm[month][6]
                 sc_val = scale(maxv, minv, battery)
-                sum += sc_val
+                power += sc_val
                 sc_row.append(sc_val)
 
                 ram = int(vals[7])
                 minv = mmm[month][7]
                 maxv = mmm[month][8]
                 sc_val = scale(maxv, minv, ram)
-                sum += sc_val
+                power += sc_val
                 sc_row.append(sc_val)
 
                 proc = float(vals[8])
                 minv = mmm[month][9]
                 maxv = mmm[month][10]
                 sc_val = scale(maxv, minv, proc)
-                sum += sc_val
+                power += sc_val
                 sc_row.append(sc_val)
 
                 wifi = 1 if (vals[9] == "Yes") else 0
                 minv = mmm[month][11]
                 maxv = mmm[month][12]
                 sc_val = scale(maxv, minv, wifi)
-                sum += sc_val
+                power += sc_val
                 sc_row.append(sc_val)
 
                 hd = int(vals[10])
                 minv = mmm[month][13]
                 maxv = mmm[month][14]
                 sc_val = scale(maxv, minv, hd)
-                sum += sc_val
+                power += sc_val
                 sc_row.append(sc_val)
 
                 apps = 1 if (vals[11] == "Yes") else 0
                 minv = mmm[month][15]
                 maxv = mmm[month][16]
                 sc_val = scale(maxv, minv, apps)
-                sum += sc_val
+                power += sc_val
                 sc_row.append(sc_val)
 
-                sc_row.insert(2, sum)
+                sc_row.insert(2, power)
 
     file_path_out = Path.cwd() / "monthly/LaptopSalesScaled.csv"
 
     with file_path_out.open(mode="w") as out:
         for row in sc:
-            # print(row)
             out.write(",".join([str(num) for num in row]))
             out.write("\n")
 
     return sc
 
-# for each laptop, rescale each component 1-10 to reflect relative "power" to other
-# laptops sold in the same month
 
-# sc (scale) is table of each sale with laptop attributes scaled 1-10
-# and a total "power" rating that is the sum
-# Each laptop is categorized as being "Low Range", "Mid Range", or "Top of the Line"
-# based on how much power it had versus other laptops sold that month
-# month price       screen      battery     ram     power   category
-# 1     395         1           8.7         5.6     15.3    Mid Range
-# 2     417         10          5.4         2.3     17.7    Low Range
+# Sam as scaled_monthly_sales, but uses the min-max values
+# for all laptops sold the whole year
 def scaled_overall_sales(omm):
+    # sc (scale) is table of each sale with laptop attributes scaled 1-10
+    # and a total "power" rating that is the sum
+    # month price       power   screen      battery     ram
+    # 1     395         15.3    1           8.7         5.6
+    # 2     417         17.7    10          5.4         2.3
     sc = []
 
     file_path = Path.cwd() / "LaptopSales.csv"
@@ -285,6 +291,8 @@ def scaled_overall_sales(omm):
             i += 1  # line number
             line = line.strip()
             vals = line.split(",")
+            # using uncleaned data file, so skip rows with
+            # missing date or price vals
             if i == 1 or vals[0] == "" or vals[4] == "":
                 x += 1
             else:
@@ -292,7 +300,7 @@ def scaled_overall_sales(omm):
 
                 sc_row = []
                 sc.append(sc_row)
-                sum = 0
+                power = 0
                 sc_row.append(month)
 
                 price = int(vals[4])
@@ -302,74 +310,74 @@ def scaled_overall_sales(omm):
                 minv = omm[1][2]
                 maxv = omm[1][3]
                 sc_val = scale(maxv, minv, screen)
-                sum += sc_val
+                power += sc_val
                 sc_row.append(sc_val)
 
                 battery = int(vals[6])
                 minv = omm[1][4]
                 maxv = omm[1][5]
                 sc_val = scale(maxv, minv, battery)
-                sum += sc_val
+                power += sc_val
                 sc_row.append(sc_val)
 
                 ram = int(vals[7])
                 minv = omm[1][6]
                 maxv = omm[1][7]
                 sc_val = scale(maxv, minv, ram)
-                sum += sc_val
+                power += sc_val
                 sc_row.append(sc_val)
 
                 proc = float(vals[8])
                 minv = omm[1][8]
                 maxv = omm[1][9]
                 sc_val = scale(maxv, minv, proc)
-                sum += sc_val
+                power += sc_val
                 sc_row.append(sc_val)
 
                 wifi = 1 if (vals[9] == "Yes") else 0
                 minv = omm[1][10]
                 maxv = omm[1][11]
                 sc_val = scale(maxv, minv, wifi)
-                sum += sc_val
+                power += sc_val
                 sc_row.append(sc_val)
 
                 hd = int(vals[10])
                 minv = omm[1][12]
                 maxv = omm[1][13]
                 sc_val = scale(maxv, minv, hd)
-                sum += sc_val
+                power += sc_val
                 sc_row.append(sc_val)
 
                 apps = 1 if (vals[11] == "Yes") else 0
                 minv = omm[1][14]
                 maxv = omm[1][15]
                 sc_val = scale(maxv, minv, apps)
-                sum += sc_val
+                power += sc_val
                 sc_row.append(sc_val)
 
-                sc_row.insert(2, sum)
+                sc_row.insert(2, power)
 
     file_path_out = Path.cwd() / "overall/LaptopSalesScaled.csv"
 
     with file_path_out.open(mode="w") as out:
         for row in sc:
-            # print(row)
             out.write(",".join([str(num) for num in row]))
             out.write("\n")
 
     return sc
 
 
-# mpmm (monthly-power-min-max) is table of power min/maxes
-# month power-min   power-max
-# 1     43.561      67.999
-# 2     44.718      68.412
+# Collects the min and max power of laptops sold in each month.
 def monthly_power_min_max():
+    # mpmm (monthly-power-min-max) is table of power min/maxes
+    # month power-min   power-max
+    # 1     43.561      67.999
+    # 2     44.718      68.412
     mpmm = []
     mpmm.append(["month", "power-min", "power-max"])
 
     for month in range(1, 13):
-        mpmm.append([1, 99999, -99999])
+        mpmm.append([1, 99999, -99999])  # default min/max
 
     file_path = Path.cwd() / "monthly/LaptopSalesScaled.csv"
 
@@ -389,16 +397,18 @@ def monthly_power_min_max():
 
     with file_path_out.open(mode="w") as out:
         for row in mpmm:
-            # print(row)
             out.write(",".join([str(num) for num in row]))
             out.write("\n")
 
     return mpmm
 
-# opmm (overall-power-min-max) is table of power min/maxes
-# power-min   power-max
-# 43.561      67.999
+
+# Same as monthly_power_min_max but find the max/min score
+# of all laptops sold that year
 def overall_power_min_max():
+    # opmm (overall-power-min-max) is table of power min/maxes
+    # power-min   power-max
+    # 43.561      67.999
     mpmm = []
     mpmm.append(["power-min", "power-max"])
     mpmm.append([99999, -99999])
@@ -425,26 +435,20 @@ def overall_power_min_max():
     return mpmm
 
 
-# for each laptop, categorize as "Low Range", "Mid Range", or "Top of the Line"
+# For each laptop, categorize as "Low Range", "Mid Range", or "Top of the Line"
 # based on how much power it had versus other laptops sold that month
-
-# cat (categorized) is table of each sale with its power rating, category, and
-# power-per-dollar
-# Each laptop is categorized as being "Low Range", "Mid Range", or "Top of the Line"
-# based on how much power it had versus other laptops sold that month
-# month price       power   power-min   power-max   category    power-per-dollar
-# 1     395         15.3    43.561      67.999      Low Range   0.0387
-# 1     417         17.7    43.561      67.999      Mid Range   0.0424
-def categoried_monthly_sales(mpmm):
+def categorized_monthly_sales(mpmm):
+    # cat (categorized) is table of each sale with its power rating, category, and
+    # power-per-dollar
+    # month price       power   power-min   power-max   category    power-per-dollar
+    # 1     395         15.3    43.561      67.999      Lower End   0.0387
+    # 1     417         17.7    43.561      67.999      Mid Range   0.0424
     cat = []
 
     file_path = Path.cwd() / "monthly/LaptopSalesScaled.csv"
 
     with file_path.open() as laptop_sales_scaled:
-        i = 0
-        x = 0
         for line in laptop_sales_scaled.readlines():
-            i += 1  # line number
             line = line.strip()
             vals = line.split(",")
 
@@ -472,31 +476,24 @@ def categoried_monthly_sales(mpmm):
 
     with file_path_out.open(mode="w") as out:
         for row in cat:
-            # print(row)
             out.write(",".join([str(num) for num in row]))
             out.write("\n")
 
 
-# for each laptop, categorize as "Low Range", "Mid Range", or "Top of the Line"
-# based on how much power it had versus other laptops sold ever
-
-# cat (categorized) is table of each sale with its power rating, category, and
-# power-per-dollar
-# Each laptop is categorized as being "Low Range", "Mid Range", or "Top of the Line"
-# based on how much power it had versus other laptops sold ever
-# month price       power   power-min   power-max   category    power-per-dollar
-# 1     395         15.3    43.561      67.999      Low Range   0.0387
-# 1     417         17.7    43.561      67.999      Mid Range   0.0424
-def categoried_overall_sales(opmm):
+# Same as categorized_monthly_sales, but uses the min/max
+# scores of all laptops sold
+def categorized_overall_sales(opmm):
+    # cat (categorized) is table of each sale with its power rating, category, and
+    # power-per-dollar
+    # month price       power   power-min   power-max   category    power-per-dollar
+    # 1     395         15.3    43.561      67.999      Lower End   0.0387
+    # 1     417         17.7    43.561      67.999      Mid Range   0.0424
     cat = []
 
     file_path = Path.cwd() / "overall/LaptopSalesScaled.csv"
 
     with file_path.open() as laptop_sales_scaled:
-        i = 0
-        x = 0
         for line in laptop_sales_scaled.readlines():
-            i += 1  # line number
             line = line.strip()
             vals = line.split(",")
 
@@ -524,7 +521,6 @@ def categoried_overall_sales(opmm):
 
     with file_path_out.open(mode="w") as out:
         for row in cat:
-            # print(row)
             out.write(",".join([str(num) for num in row]))
             out.write("\n")
 
@@ -533,10 +529,10 @@ if __name__ == '__main__':
     mmm = monthly_min_max()
     sc = scaled_monthly_sales(mmm)
     mpmm = monthly_power_min_max()
-    cat = categoried_monthly_sales(mpmm)
+    cat = categorized_monthly_sales(mpmm)
     # omm = overall_min_max()
     # sc = scaled_overall_sales(omm)
     # opmm = overall_power_min_max()
-    # cat = categoried_overall_sales(opmm)
+    # cat = categorized_overall_sales(opmm)
 
 
